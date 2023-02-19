21014cbf876f34212b72d324efdbd101f50f563f
Davor Telisman <davortelisman@gmail.com>
2023-02-19 18:42:30 +0100

    Create models country and import values using shell in are terminal, to import Countrys by code and name

--- podcast_user/models.py
+++ podcast_user/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin,AbstractUser
+from django.core.validators import RegexValidator
+from datetime import date
 
-# Create your models here.
+
+class Country(models.Model):
+    country_id = models.AutoField(primary_key=True)
+    country_name = models.CharField(max_length=100)
+    country_code = models.CharField(max_length=3)
+
+    def __str__(self):
+        return self.country_name
