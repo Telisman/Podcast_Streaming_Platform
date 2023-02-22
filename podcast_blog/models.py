6d0b226d6123261d894fda1e444358e9f2312ae2
Davor Telisman <davortelisman@gmail.com>
2023-02-22 12:35:30 +0100

    Create new app for are project: podcast_blog 1. new model of  podcast_blog 2.new urls.py 3.Forms for blog 4.View 5. edit in to pages: timeline, and profile 6. create comments and likes for blog

--- podcast_blog/models.py
+++ podcast_blog/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from datetime import date
+from datetime import date,datetime
 from podcast_user.models import PodcastUser
 
 class Podcast_Blog(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=500, default="Post something!! (xc")
-    date_of_blog = models.DateField(default=date.today)
+    time_of_blog = models.DateTimeField(default=datetime.now)
     blog_text = models.TextField(default="Must be something")
     blog_user = models.ForeignKey(PodcastUser,on_delete=models.CASCADE,blank=False, null=False)
 
--- podcast_user/views.py
+++ podcast_user/views.py
@@ -3,9 +3,10 @@ from django.shortcuts import render, redirect,get_object_or_404
 from .forms import CustomUserCreationForm, CustomAuthenticationForm
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import PodcastUser, UserInfo
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta,date
 from django.views.generic import ListView
-
+from django.contrib.auth.decorators import login_required
+from podcast_blog.models import Podcast_Blog
 
 def Navbar_footer_menu(request):
     return render(request, "pages/navbar_footer_menu.html", {})
@@ -41,13 +42,20 @@ def PodcastDashboard(request):
 
 
 
+#
+# def profile_page(request):
+#     if request.user.is_authenticated:
+#         user_info = UserInfo.objects.get(user=request.user)
+#         return render(request, 'pages/users/profile.html', {'user': request.user, 'user_info': user_info})
+#     else:
+#         return redirect('login')
 
+@login_required
 def profile_page(request):
-    if request.user.is_authenticated:
-        user_info = UserInfo.objects.get(user=request.user)
-        return render(request, 'pages/users/profile.html', {'user': request.user, 'user_info': user_info})
-    else:
-        return redirect('login')
+    user_info = UserInfo.objects.get(user=request.user)
+    one_year_ago = date.today() - timedelta(days=365)
+    blog_posts = Podcast_Blog.objects.filter(blog_user=request.user, time_of_blog__gte=one_year_ago).order_by('-time_of_blog')
+    return render(request, 'pages/users/profile.html', {'user': request.user, 'user_info': user_info, 'blog_posts': blog_posts})
 
 def users_list(request):
     return render(request,'pages/users/contacts.html')
