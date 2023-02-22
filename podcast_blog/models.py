88f30b607a0e173d67bd80621a9ed7a043af53dc
Davor Telisman <davortelisman@gmail.com>
2023-02-22 10:48:53 +0100

    Create new app for are project: podcast_blog 1. new model of  podcast_blog 2.new urls.py 3.Forms for blog 4.View 5. edit in to pages: timeline, and profile 6. create comments and likes for blog

--- podcast_blog/models.py
+++ podcast_blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 from datetime import date
+from podcast_user.models import PodcastUser
+
+class Podcast_Blog(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=500, default="Post something!! (xc")
+    date_of_blog = models.DateField(default=date.today)
+    blog_text = models.TextField(default="Must be something")
+    blog_user = models.ForeignKey(PodcastUser,on_delete=models.CASCADE,blank=False, null=False)
+
+    def __str__(self):
+        return f"{self.id} , {self.name}, {self.blog_user} "
 
