a73295c8fb2c5062e2e94815bf4b503de059e22c
Davor Telisman <davortelisman@gmail.com>
2023-02-28 00:03:47 +0100

    timeline page: add blog form, add delete blog

--- podcast_blog/urls.py
+++ podcast_blog/urls.py
@@ -4,4 +4,6 @@ from .views import blog_timeline,PodcastBlogCreateView,BlogDeleteView
 urlpatterns = [
     path("timeline/",blog_timeline,name='timeline'),
     path('add_blog/<int:pk>/', PodcastBlogCreateView.as_view(), name='create_blog'),
-    path('blog/<int:pk>/delete/', BlogDeleteView.as_view(), name='blog-delete'),]
+    path('blog/<int:pk>/delete/', BlogDeleteView.as_view(), name='blog-delete'),
+
+]
