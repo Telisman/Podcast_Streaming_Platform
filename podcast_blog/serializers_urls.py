499d0b33543a7382672ad49de084bed9c7bfd90a
Davor Telisman <davortelisman@gmail.com>
2023-03-11 14:23:34 +0100

    Create API PUT,DELETE for blog and comment

--- podcast_blog/serializers_urls.py
+++ podcast_blog/serializers_urls.py
@@ -9,7 +9,9 @@ urlpatterns = [
     path('comments/', serializers_view.api_detail_comments_view),
     path('comment/<int:pk>/', serializers_view.api_detail_comment_view),
     #edit
-    path('blogs/', serializers_view.api_detail_blogs_view),
-
+    path('blog/edit/<int:pk>/', serializers_view.api_update_blog_view),
+    path('comment/edit/<int:pk>/', serializers_view.api_update_comment_view),
     #delete
+    path('blog/delete/<int:pk>/', serializers_view.api_delete_blog),
+    path('comment/delete/<int:pk>/', serializers_view.api_delete_comment),
 ]
--- podcast_blog/serializers_view.py
+++ podcast_blog/serializers_view.py
@@ -16,12 +16,12 @@ from django.contrib.auth.decorators import login_required
 @api_view(['GET'])
 def api_detail_blogs_view(request):
     try:
-        users = Podcast_Blog.objects.all()
+        blogs = Podcast_Blog.objects.all()
     except Podcast_Blog.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
-        serializer = Podcast_BlogSerializer(users, many=True) # set many=True
+        serializer = Podcast_BlogSerializer(blogs, many=True) # set many=True
         return Response(serializer.data)
 
 # List of data for only one blog set by PK
@@ -29,24 +29,24 @@ def api_detail_blogs_view(request):
 def api_detail_blog_view(request, pk):
 
     try:
-        users = Podcast_Blog.objects.get(pk=pk)
+        blog = Podcast_Blog.objects.get(pk=pk)
     except Podcast_Blog.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == "GET":
-        serializers = Podcast_BlogSerializer(users)
+        serializers = Podcast_BlogSerializer(blog)
         return Response(serializers.data)
 
 # List of data from  all comment
 @api_view(['GET'])
 def api_detail_comments_view(request):
     try:
-        users = BlogComment.objects.all()
+        comments = BlogComment.objects.all()
     except BlogComment.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
-        serializer = BlogCommentSerializer(users, many=True) # set many=True
+        serializer = BlogCommentSerializer(comments, many=True) # set many=True
         return Response(serializer.data)
 
 # List of data for only one comment set by PK
@@ -54,10 +54,77 @@ def api_detail_comments_view(request):
 def api_detail_comment_view(request, pk):
 
     try:
-        users = BlogComment.objects.get(pk=pk)
+        comment = BlogComment.objects.get(pk=pk)
     except BlogComment.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == "GET":
-        serializers = BlogCommentSerializer(users)
+        serializers = BlogCommentSerializer(comment)
         return Response(serializers.data)
+
+
+# edit blog
+@api_view(['PUT'])
+@login_required
+def api_update_blog_view(request, pk):
+    try:
+        blog = Podcast_Blog.objects.get(pk=pk)
+    except Podcast_Blog.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'PUT':
+        serializer = Podcast_BlogSerializer(blog, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# edit comment
+@api_view(['PUT'])
+@login_required
+def api_update_comment_view(request, pk):
+    try:
+        comment = BlogComment.objects.get(pk=pk)
+    except BlogComment.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'PUT':
+        serializer = BlogCommentSerializer(comment, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#     delete blog
+@api_view(['DELETE'])
+@login_required
+def api_delete_blog(request, pk):
+    try:
+        blog = Podcast_Blog.objects.get(pk=pk)
+    except Podcast_Blog.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'DELETE':
+        operation = blog.delete()
+        data = {}
+        if operation:
+            data["success"] = "delete successful"
+        else:
+            data["failure"] = "delete failed"
+        return Response(data=data)
+
+#delete comment
+@api_view(['DELETE'])
+@login_required
+def api_delete_comment(request, pk):
+    try:
+        comment = BlogComment.objects.get(pk=pk)
+    except BlogComment.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'DELETE':
+        operation = comment.delete()
+        data = {}
+        if operation:
+            data["success"] = "delete successful"
+        else:
+            data["failure"] = "delete failed"
+        return Response(data=data)
